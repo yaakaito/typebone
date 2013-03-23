aa9cf4747b43775a664fd90061f3e8d1f1353db0
KAZUMA Ukyo <yaakaito@gmail.com>
2013-03-24 05:55:49 +0900

    refactor Events

--- src/events.ts
+++ src/events.ts
@@ -1,29 +1,76 @@
 /// <reference path="../vendor/underscore.d.ts" />
 
 module Backbone {
+
+  class Event {
+    
+    callbacks : Function[] = [];
+    name : string;
+
+    constructor(name : string) {
+      this.name = name;
+    }
+
+    addCallback(callback : Function) {
+      this.callbacks.push(callback);
+    }
+
+    fire() : void {
+      _.each(this.callbacks, function(callback : Function){
+        callback();
+      });
+    }
+  }
+
+  class EventList {
+    events : Event[] = [];
+
+    private get(name : string) : Event {
+      return this.events[name];
+    }
+
+    private createEvent(name : string, callback : Function) : void {
+      var theEvent = new Event(name);
+      theEvent.addCallback(callback);
+      this.events[name] = theEvent;
+    }
+
+    private registerCallback(name : string, callback : Function) : void {
+      this.get(name).addCallback(callback);
+    }
+
+    register(name : string, callback : Function) : void {
+      if (this.has(name)) {
+        this.registerCallback(name, callback);
+      }
+      else {
+        this.createEvent(name, callback);
+      }
+    } 
+
+    has(name : string) : bool {
+      return this.get(name)? true : false;
+    }
+
+    fire(name : string) : void {
+      this.get(name).fire();
+    }
+  }
+
+  /*
+   *  Backbone.Events
+   */
   export class Events {
 
-    events : array = null;
+    eventList : EventList = new EventList();
 
     on(name: string, callback: Function) : Events {
-      this.events = this.events || (this.evetns = {});
-      var theEvents = this.events[name] || (this.events[name] = [])
-      theEvents.push(callback);
-
+      this.eventList.register(name, callback);
       return this;
     }
 
     trigger(name: string) : Events {
-      if (!this.events) { 
-        return this;
-      }
-
-      var theEvents = this.events[name];
-
-      _.each(theEvents, function(callback: Function) {
-        callback();
-      });
-
+      this.eventList.fire(name);
       return this;
     }
   }
