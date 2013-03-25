52086557b8fa2e3ebf979cfca08b28bcf0ab8c3c
KAZUMA Ukyo <yaakaito@gmail.com>
2013-03-26 04:56:43 +0900

    eventable model

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,24 @@
-module Backbone {
-  export class Model {
+/// <reference path="events.ts" />
 
+module Backbone {
+  export class Model implements Eventable {
+    /*
+     * Eventable
+     */
+    private events = Backbone.Events;
+    on(events : any, callback : any, context? : any) : any {
+      this.events.on(events, callback, context);
+      return this;
+    }
+  
+    off(events : any, callback? : any, context? : any) : any {
+      this.events.off(events, callback, context);
+      return this;
+    }
+
+    trigger(name : string) : any {
+      this.events.trigger(name);
+      return this;
+    }
   }
 }
